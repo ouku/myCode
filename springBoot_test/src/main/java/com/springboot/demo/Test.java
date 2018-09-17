e64249ae817bfc0eaab39a12d3f084078ffda07a
LM <LM@DESKTOP-HHLHB15>
2018-09-17 20:12:41 +0800

    测试增加一个类Test.java

---
+++ springBoot_test/src/main/java/com/springboot/demo/Test.java
@@ -0,0 +1,46 @@
+package com.springboot.demo;
+
+/**
+ * 线程实现方式一 ： 继承Thread类
+ * @author licp
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		MThread t =  new MThread();
+		Thread t1 = new Thread(t,"PP");
+		Thread t2 = new Thread(t,"yy");
+		t1.start();
+		t2.start();
+		System.out.println("==================");
+	}
+}
+
+class MThread extends Thread{
+	 private int num = 0; // 出票数
+	    private int count = 10; // 剩余票数
+	    boolean flag = false;
+	    @Override
+	    public void run() {
+	 
+	        while (true) {
+	            synchronized (this) {
+	            	System.out.println("--");
+	                // 没有余票时，跳出循环
+	                if (count <= 0) {
+	                    break;
+	                }
+	                num++;
+	                count--;
+	                try {
+	                    Thread.sleep(10);// 模拟网络延时
+	                } catch (InterruptedException e) {
+	                    // TODO Auto-generated catch block
+	                    e.printStackTrace();
+	                }
+	                System.out.println("显示出票信息：" + Thread.currentThread().getName()
+	                        + "抢到第" + num + "张票，剩余" + count + "张票");
+	            }
+	        }
+	    }
+}
